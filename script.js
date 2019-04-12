b1ebc01649a66e6bd7ddecb1be20f75969978a01
jodymoore <jodymoore@g.harvard.edu>
2019-04-12 08:03:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -260,4 +260,10 @@ function deleteShoppingCart(position) {
 }
 
 
+function about()
+{
+    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    
+}
+
 
